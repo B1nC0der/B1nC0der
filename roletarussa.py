3b40a7722b11da5a9f90441157189fa07a075c49
B1nC0der <158519059+B1nC0der@users.noreply.github.com>
2024-02-01 18:06:16 -0300

    Create roletarussa.py

---
+++ roletarussa.py
@@ -0,0 +1,3 @@
+esse é um projeto básico de programação feito em python, porem altamente de risco
+não me responsabilizo pelas ações cometidas com isso
+este código foi criado para fins educacionais
